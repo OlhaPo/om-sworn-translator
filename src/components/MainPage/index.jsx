270f827a3680d96186d212daec281b920b3e4a84
Olya <ponomaryova.olya.y@gmail.com>
2025-05-15 17:39:11 +0200

    feat: add skeleton for home page on mobile screens

--- src/components/MainPage/index.jsx
+++ src/components/MainPage/index.jsx
@@ -2,36 +2,46 @@ import Image from 'next/image';
 
 export default function MainPage() {
   return (
-    <section className="flex justify-between my-16 max-w-[1200px] mx-auto">
-      <div className="w-1/2 text-center">
+    <section className="flex flex-col md:flex-row justify-center md:justify-between md:my-16 md:max-w-[1200px] md:mx-auto">
+      <div className="md:w-1/2 text-center">
         <Image
           src="/logo.svg"
           alt="logo"
           width={180}
           height={180}
-          className="mx-auto mb-12"
+          className="mx-auto md:mb-12 hidden md:block"
         />
         <h1
-          className="text-6xl mb-5 text-[#704B40] text-shadow-lg"
+          className="text-5xl md:text-6xl mt-20 md:mt-0 mb-2 md:mb-5 text-[#704B40] text-shadow-lg"
           style={{ fontFamily: 'var(--font-el-messiri)' }}
         >
           <span style={{ fontFamily: 'var(--font-ballet)' }}>O</span>лена{' '}
           <span style={{ fontFamily: 'var(--font-ballet)' }}>M</span>арченко
         </h1>
         <h2
-          className="text-4xl text-[#704B40] mb-12 "
+          className="text-3xl md:text-4xl text-[#704B40] mb-3 md:mb-12 "
           style={{ fontFamily: 'var(--font-el-messiri)' }}
         >
           присяжний перекладач <br /> української мови
         </h2>
+        <div className="w-full md:hidden">
+          <Image
+            src="/hero-img.jpg"
+            width={536}
+            height={684}
+            alt="image of Olena Marchenko sworn translator"
+            priority
+            className="shadow-img"
+          />
+        </div>
         <p
-          className="text-4xl text-[#926D4C] mt-16 tracking-wide"
+          className="text-4xl mb-10 md:mb-0 text-[#926D4C] mt-16 tracking-wide"
           style={{ fontFamily: 'var(--font-great-vibes)' }}
         >
-          &quot; Законність відкриває двері до можливостей &quot;
+          &quot; Законність відкриває двері до&nbsp;можливостей &quot;
         </p>
       </div>
-      <div className="w-[40%]">
+      <div className="hidden md:block md:w-[40%]">
         <Image
           src="/hero-img.jpg"
           width={536}
--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -1,12 +1,24 @@
+'use client';
+
+import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
+import { HiOutlineMenu } from 'react-icons/hi';
+import { IoCloseOutline } from 'react-icons/io5';
 
 import useNavLinks from './hooks/useNavLinks';
 
 export default function Header() {
   const links = useNavLinks();
+
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const handleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
   return (
     <nav className="custom-shadow relative bg-[#A98573] text-white">
-      <div className="flex justify-between py-6 max-w-[1200px] mx-auto">
+      <div className="w-full hidden md:flex md:justify-between md:py-6 md:max-w-[1200px] md:mx-auto">
         <ul className="flex text-[16px]">
           <li>UK </li>
           <span className="px-2">|</span>
@@ -20,6 +32,36 @@ export default function Header() {
           ))}
         </ul>
       </div>
+      <div className="flex justify-between px-8 py-2">
+        <Image
+          src="/logo.svg"
+          alt="logo"
+          width={100}
+          height={100}
+          className="md:hidden"
+        />
+        <button onClick={handleMobileMenu} className="md:hidden">
+          <HiOutlineMenu size={30} />
+        </button>
+      </div>
+
+      {isMobileMenuOpen && (
+        <div className="flex flex-col items-center justify-center">
+          <IoCloseOutline size={30} />
+          <ul className="flex flex-col text-[16px] uppercase">
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link href={link.href}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+          <ul className="flex text-[16px]">
+            <li>UK </li>
+            <span className="px-2">|</span>
+            <li>PL</li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
