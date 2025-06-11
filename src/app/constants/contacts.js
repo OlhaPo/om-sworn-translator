35e6ca4cc5c1c4ce7f43db403bf2cddf8f5a0fc3
Olya <ponomaryova.olya.y@gmail.com>
2025-06-11 18:08:44 +0200

    chore: add mail to next public variable

--- src/app/constants/contacts.js
+++ src/app/constants/contacts.js
@@ -2,7 +2,7 @@ export const FACEBOOK_LINK = 'https://www.facebook.com/share/1CJZQuekqu/';
 export const INSTAGRAM_LINK = 'https://www.instagram.com/';
 export const VIBER_LINK = 'viber://chat?number=%2B48514249213';
 export const PHONE_NUMBER = '+48514249213';
-export const EMAIL = 'tlumaczmarchenko@gmail.com';
+export const EMAIL = process.env.NEXT_PUBLIC_EMAIL;
 export const ADDRESS_STREET = 'ul.Tadeusza Romanowicza 15, ';
 export const ADDRESS_POSTAL_CODE = '43-300 Bielsko-Biała';
 export const GOOGLE_MAP_LOCATION = 'https://maps.app.goo.gl/j3QEtuQtQR1QazV3A';
